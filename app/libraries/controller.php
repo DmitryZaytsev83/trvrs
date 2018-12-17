b94c7de336d8dc33157c03cd8632763801146a40
Dmitry Zaytsev <dmitry.zaytsev83@gmail.com>
2018-12-17 16:26:37 +0300

    create base structure of app

---
+++ app/libraries/controller.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dmitr
+ * Date: 17.12.2018
+ * Time: 16:14
+ */
---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+echo "Hello World!";
+/**
+ * Created by PhpStorm.
+ * User: Dmitr
+ * Date: 17.12.2018
+ * Time: 16:10
+ */
